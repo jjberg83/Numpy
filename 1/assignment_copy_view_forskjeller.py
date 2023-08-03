6d728db579fbac5327f9ceda40e58996d52ecc71
Jørund Jenssen Berg <jjberg@gmail.com>
2023-08-03 15:47:16 +0200

    Forklaring i kode på forskjell mellom assignment, copy og view

---
+++ 1/assignment_copy_view_forskjeller.py
@@ -0,0 +1,15 @@
+﻿import numpy as np
+
+theOriginal = np.array([1,2,3,4])
+theAssignment = theOriginal
+theCopy = theOriginal.copy()
+theView = theOriginal.view()
+
+theAssignment[1] = 22
+theCopy[2] = 33
+theView[3] = 44
+
+print(id(theOriginal), "theOriginal", theOriginal)
+print(id(theAssignment), "theAssignment", theAssignment)
+print(id(theCopy), "theCopy", theCopy)
+print(id(theView), "theView", theView)
