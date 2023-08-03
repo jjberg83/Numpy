cc26fe64989d4d8115dc6b9adb4546df0dbd23de
Jørund Jenssen Berg <jjberg@gmail.com>
2023-08-03 15:57:31 +0200

    La til base

--- 1/assignment_copy_view_forskjeller.py
+++ 1/assignment_copy_view_forskjeller.py
@@ -9,7 +9,7 @@ theAssignment[1] = 22
 theCopy[2] = 33
 theView[3] = 44
 
-print(id(theOriginal), "theOriginal", theOriginal)
-print(id(theAssignment), "theAssignment", theAssignment)
-print(id(theCopy), "theCopy", theCopy)
-print(id(theView), "theView", theView)
+print(id(theOriginal), "theOriginal", theOriginal, "base:", theOriginal.base)
+print(id(theAssignment), "theAssignment", theAssignment, "base:", theAssignment.base)
+print(id(theCopy), "theCopy", theCopy, "base:", theCopy.base)
+print(id(theView), "theView", theView, "base:", theView.base)
